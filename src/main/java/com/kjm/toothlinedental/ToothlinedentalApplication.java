72f622584afdb8ada3f4c83b7e06970c9563b6ef
Kate Martin <katejaneen23@gmail.com>
2025-06-24 02:01:10 +0800

    Initial commit

---
+++ src/main/java/com/kjm/toothlinedental/ToothlinedentalApplication.java
@@ -0,0 +1,13 @@
+package com.kjm.toothlinedental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToothlinedentalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToothlinedentalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kjm/toothlinedental/ToothlinedentalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kjm.toothlinedental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToothlinedentalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
